91be4f7a0d41e2fc9022966bdd449f5b73782518
Austin Kien Hoang <46822388+devhoangkien@users.noreply.github.com>
2024-04-08 17:22:32 +0700

    Update index.js

--- example/index.js
+++ example/index.js
@@ -7,13 +7,14 @@ const root = createRoot(container);
 
 const onSuccess = (response) => console.log(response);
 const onFailure = (response) => console.error(response);
-
+# làm 1 hàm để post ai post zo
+# call back
 root.render(
   <LoginOAuth2
-    authorizeUri="https://discord.com/oauth2/authorize"
+    authorizeUri="https://staging.aixblock.org/o/authorize/"
     responseType="code"
-    clientId="XXXXX"
-    redirectUri=""
+    clientId="25sTUN74v3sCfDiDGIRQxNHrFfEgN855RglYbX4w"
+    redirectUri="http://108.181.196.144:8100/hub/oauth_callback"
     scope="identify guilds"
     onSuccess={onSuccess}
     onFailure={onFailure}
